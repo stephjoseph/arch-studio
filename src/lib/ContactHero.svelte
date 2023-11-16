f62f01f789473c6b8d585e447777ca3fd9f523df
Stephen Muñez <sjtmunez@gmail.com>
2023-11-16 09:52:34 +0800

    feat: contact hero mobile

---
+++ src/lib/ContactHero.svelte
@@ -0,0 +1,107 @@
+<script>
+    import { onMount } from "svelte";
+    import imgHeroMobile from "../assets/contact/mobile/image-hero.jpg";
+    import imgHeroTablet from "../assets/contact/tablet/image-hero.jpg";
+    import imgHeroDesktop from "../assets/contact/desktop/image-hero.jpg";
+  
+    let windowWidth = window.innerWidth;
+  
+    // Update window width when the window is resized
+    onMount(() => {
+      window.addEventListener("resize", () => {
+        windowWidth = window.innerWidth;
+      });
+  
+      return () => {
+        window.removeEventListener("resize", () => {});
+      };
+    });
+  
+    // Define different image sources based on window width
+    $: bgImgSrc =
+      windowWidth >= 1280
+        ? imgHeroDesktop
+        : windowWidth >= 768
+        ? imgHeroTablet
+        : imgHeroMobile;
+  </script>
+  
+  <section class="contact-hero">
+    <div class="contact-hero__img" style="background-image: url({bgImgSrc})">
+      <div></div>
+    </div>
+    <div class="contact-hero__content">
+      <div class="contact-hero__content-wrapper">
+        <h2>Your team of professionals</h2>
+        <p>
+          Our small team of world-class professionals will work with you every
+          step of the way. Strong relationships are at the core of everything we
+          do. This extends to the relationship our projects have with their
+          surroundings.
+        </p>
+      </div>
+    </div>
+  </section>
+  
+  <style lang="scss">
+    .contact-hero {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+  
+      &__img {
+        width: 100%;
+        height: 64vw;
+        background: rgba(0, 0, 0, 0.35);
+        background-repeat: no-repeat;
+        background-size: 100%;
+        background-position: bottom;
+        background-blend-mode: multiply;
+        transition-property: background-size;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 600ms;
+  
+        &:hover {
+          background-size: 120%;
+        }
+  
+        div {
+          height: 64vw;
+        }
+      }
+  
+      &__content {
+        width: 100%;
+        padding-right: 32px;
+        background: transparent;
+        margin-top: -45px;
+  
+        &-wrapper {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          gap: 20px;
+          background: #fff;
+          padding: 65px 0px 76px 32px;
+  
+          h2 {
+            color: var(--very-dark-blue);
+            font-size: 48px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 52px;
+            letter-spacing: -1.2px;
+          }
+  
+          p {
+            color: #60636d;
+            font-size: 18px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 24px;
+          }
+        }
+      }
+    }
+  </style>
+  
--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,12 +1,26 @@
 <script>
-    // your script goes here
+import ContactHero from "../lib/ContactHero.svelte";
 </script>
 
 <main>
     <h1>Contact</h1>
+    <ContactHero/>
 </main>
 
-<style>
-    /* your styles go here */
-</style>
+<style lang="scss">
+    main {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background: #fff;
+  
+      h1 {
+        position: absolute;
+        visibility: hidden;
+        width: 0;
+        height: 0;
+      }
+    }
+  </style>
 
