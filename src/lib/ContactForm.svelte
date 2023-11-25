e9b0ef40199f81ed4ce6b9fb0957918f50a0a9e9
Stephen Muñez <sjtmunez@gmail.com>
2023-11-25 09:50:25 +0800

    feat: form validation

--- src/lib/ContactForm.svelte
+++ src/lib/ContactForm.svelte
@@ -1,19 +1,91 @@
 <script>
   import iconArrowWhite from "../assets/icons/icon-arrow-white.svg";
+
+  let formData = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  let errors = {};
+
+  function handleSubmit() {
+    errors = {}; // Reset errors
+
+    // Validation checks
+    if (!formData.name.trim()) {
+      errors.name = "Can’t be empty";
+    }
+
+    if (!formData.email.trim()) {
+      errors.email = "Can’t be empty";
+    } else if (!isValidEmail(formData.email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!formData.message.trim()) {
+      errors.message = "Can’t be empty";
+    }
+
+    // If there are no errors, log the form data
+    if (Object.keys(errors).length === 0) {
+      console.log(formData);
+    }
+  }
+
+  function isValidEmail(email) {
+    // Basic email validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 </script>
 
 <section class="contact-form">
   <h2>Connect with us</h2>
-  <form class="contact-form__form">
-    <input type="text" name="name" id="name" placeholder="Name" />
-    <input type="email" name="email" id="email" placeholder="Email" />
-    <textarea
-      name="message"
-      id="message"
-      cols="15"
-      rows="5"
-      placeholder="Message"
-    ></textarea>
+  <form
+    on:submit|preventDefault={handleSubmit}
+    novalidate
+    class="contact-form__form"
+  >
+    <div class="contact-form__input-group">
+      <input
+        bind:value={formData.name}
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Name"
+        class:invalid={errors.name}
+        class:valid={!errors.name && formData.name.trim()}
+      />
+      {#if errors.name}<p class="contact-form__error">{errors.name}</p>{/if}
+    </div>
+    <div class="contact-form__input-group">
+      <input
+        bind:value={formData.email}
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email"
+        class:invalid={errors.email}
+        class:valid={!errors.email && formData.email.trim()}
+      />
+      {#if errors.email}<p class="contact-form__error">{errors.email}</p>{/if}
+    </div>
+    <div class="contact-form__input-group">
+      <textarea
+        bind:value={formData.message}
+        name="message"
+        id="message"
+        cols="15"
+        rows="5"
+        placeholder="Message"
+        class:invalid={errors.message}
+        class:valid={!errors.message && formData.message.trim()}
+      ></textarea>
+      {#if errors.message}<p class="contact-form__error">
+          {errors.message}
+        </p>{/if}
+    </div>
     <button type="submit"><img src={iconArrowWhite} alt="arrow icon" /></button>
   </form>
 </section>
@@ -48,7 +120,7 @@
         border: none;
         border-bottom: 1px solid var(--very-dark-blue);
         color: var(--very-dark-blue);
-        padding: 0px 24px 32px;
+        padding: 0px 24px 24px;
         font-size: 24px;
         font-style: normal;
         font-weight: 700;
@@ -61,6 +133,18 @@
 
         &::placeholder {
           color: var(--light-grey);
+        }
+
+        &.valid {
+          border-width: 3px;
+        }
+
+        &.invalid {
+          border-color: var(--red-error);
+
+          &::placeholder {
+            color: rgba(223, 86, 86, 0.5);
+          }
         }
       }
 
@@ -85,6 +169,17 @@
         &::placeholder {
           color: var(--light-grey);
         }
+
+        &.valid {
+          border-width: 3px;
+        }
+        &.invalid {
+          border-color: var(--red-error);
+
+          &::placeholder {
+            color: rgba(223, 86, 86, 0.5);
+          }
+        }
       }
 
       button {
@@ -95,7 +190,7 @@
         justify-content: center;
         border: none;
         position: absolute;
-        bottom: -80px;
+        bottom: -76px;
         right: 0;
         cursor: pointer;
 
@@ -107,6 +202,21 @@
           background: var(--light-grey);
         }
       }
+    }
+
+    &__input-group {
+      width: 100%;
+      position: relative;
+    }
+
+    &__error {
+      position: absolute;
+      color: var(--red-error);
+      font-size: 18px;
+      font-weight: 700;
+      letter-spacing: -0.281px;
+      right: 16px;
+      bottom: 16px;
     }
   }
 
